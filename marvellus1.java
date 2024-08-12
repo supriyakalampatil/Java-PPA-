2f00eaffe14af6122134d8576acdc60b4dc80a6c
supriyakalampatil <85495251+supriyakalampatil@users.noreply.github.com>
2024-08-12 18:24:30 +0530

    Add files via upload

---
+++ marvellus1.java
@@ -0,0 +1,19 @@
+import java.lang.*;
+
+interface circle
+{
+	float PI =3.14;
+	
+	float calculateArea(float radius);
+	float calculateCircumfarance(float radius);
+	
+}
+
+class marvellus1
+{
+	public static void main(string arg[])
+	{
+	}
+	{
+		
+
